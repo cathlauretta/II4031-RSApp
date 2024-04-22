3170a81440cb5476e2caeae449a68524007a034e
cathlauretta <18221157@std.stei.itb.ac.id>
2024-04-22 23:24:48 +0700

    feat: add random bigint and readme

--- src/utils/rsa.tsx
+++ src/utils/rsa.tsx
@@ -1,3 +1,5 @@
+import { decode } from "punycode";
+
 function randomBigIntFromInterval(min: bigint, max: bigint): bigint {
     const range = max - min + BigInt(1);
     const randomNumber = BigInt(Math.floor(Number(range) * Math.random()));
@@ -155,9 +157,12 @@ function bigIntToNumber(bigInts: bigint[]): number[] {
 // const decode_base64 = (decodeBase64(base64cipher));
 // console.log(`The decoded message is: ${decode_base64}`);
 
-export function generateKeys(): bigint[] {
-    const p = BigInt(47);
-    const q = BigInt(71);
+const primeNumber = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+
+export function generateKeys(): bigint[] { 
+    const p = BigInt(primeNumber[Math.floor(Math.random() * primeNumber.length)]);
+    const q = BigInt(primeNumber[Math.floor(Math.random() * primeNumber.length)]);
+    console.log(`The p is: ${p}; The q is: ${q}`);
     const n = p * q;
     const totient = (p - BigInt(1)) * (q - BigInt(1));
     
@@ -181,7 +186,6 @@ export function decryption(arrInt: number[], d: bigint, n: bigint): string {
     const cipher = numberToBigInt(arrInt);
     const decryptedResult = encrypt(cipher, [d, n]);
     const result = bigIntToNumber(decryptedResult);
-    console.log(result);
     const base64result = integerArrayToBase64String(result);
     const message = decodeBase64(base64result);
 
